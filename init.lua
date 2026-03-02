3af94b3301893f82480af70be9cc15bafe407dda
Turtolo <Lauvstadnils4@gmail.com>
2026-03-02 20:36:32 +0100

    Commit

--- init.lua
+++ init.lua
@@ -64,14 +64,10 @@ local opts = {}
 
 require("lazy").setup(plugins, opts)
 
-local opts = {}
-
-require("lazy").setup(plugins, opts)
-
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
-vim.keymap.set('n', '<C-g>', ':Neotree <CR>', {})
+vim.keymap.set('n', '<C-n>', ':Neotree <CR>', {})
 
 require("gruvbox").setup()
 vim.cmd.colorscheme "gruvbox"
